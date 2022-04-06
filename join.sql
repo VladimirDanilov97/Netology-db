5dec3138fafd61b14321dff6c7eda45192ed661e
VladimirDanilov97 <dan1997ford@yandex.ru>
2022-04-06 19:46:12 +0300

    fix mistakes in sql

--- join.sql
+++ join.sql
@@ -34,19 +34,18 @@ join artist on aa.artist_id = artist.id
 where artist_name = 'Placebo';
 
 --наименование треков, которые не входят в сборники;
-select track_name from track 
-where track_name not in (
-select track_name from collection c
-join collection_track ct on c.id = ct.collection_id
-join track t on ct.track_id = t.id);
+select track_name  from track 
+left join collection_track ct on track.id = ct.track_id
+where ct.track_id is null;
 
 --исполнителя(-ей), написавшего самый короткий по продолжительности трек (теоретически таких треков может быть несколько);
+with grouped as(
 select artist_name, duration from track t 
 join album a on t.album_id = a.id 
 join artist_album aa on a.id = aa.album_id 
-join artist a2 on aa.artist_id = a2.id 
-order by duration 
-limit 1; 
+join artist a2 on aa.artist_id = a2.id)
+select artist_name, duration from grouped
+where duration = (select MIN(duration) from track);
 
 --название альбомов, содержащих наименьшее количество треков.
 with grouped as( select a.album_name, count(*) track_number from track t
@@ -56,13 +55,12 @@ select * from grouped
 where track_number = (select Min(track_number) from grouped);
 
 --название альбомов, в которых присутствуют исполнители более 1 жанра;
-with grouped as (select album_name, count(category_name) cat_count from album al
+select album_name, count(category_name) cat_count from album al
 join artist_album aa on al.id = aa.album_id
 join artist_category ac on aa.artist_id = ac.artist_id 
 join category c on ac.category_id = c.id
-group by album_name)
-select * from grouped 
-where cat_count > 1;
+group by album_name
+having COUNT(distinct category_name) > 1;
 
 
 
